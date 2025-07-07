8e35819602f19ebc0f564b28a069a72ed1a5dc17
Milk-Cool <coder.tomsk@gmail.com>
2025-07-07 21:39:28 +0300

    send collected macs as strings

--- src/api.cpp
+++ src/api.cpp
@@ -3,14 +3,18 @@
 #include <ArduinoJson.h>
 #include "esp_wifi.h"
 
-String get_mac() {
-    uint8_t mac[6];
-    esp_wifi_get_mac(WIFI_IF_STA, mac);
+String get_mac_str(uint8_t* mac) {
     char mac_str[18];
     sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x",
         mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     mac_str[17] = '\0';
     return String(mac_str);
+}
+
+String get_mac() {
+    uint8_t mac[6];
+    esp_wifi_get_mac(WIFI_IF_STA, mac);
+    return get_mac_str(mac);
 }
 
 void api_send_sniff(String base, std::vector<Sniff> sniffs) {
@@ -27,8 +31,7 @@ void api_send_sniff(String base, std::vector<Sniff> sniffs) {
     for(Sniff sn : sniffs) {
         JsonDocument doc;
         JsonArray mac;
-        for(int i = 0; i < 6; i++)
-            doc["mac"].add(sn.mac[i]);
+        doc["mac"] = get_mac_str(sn.mac);
         doc["rssi"] = sn.rssi;
         data["sniffs"].add(doc);
     }
